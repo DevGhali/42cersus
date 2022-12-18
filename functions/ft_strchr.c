89ecfb5a31ef53928617b3cb3b29b8e11a99b998
Ghali Abd El-Hai <gabd-el-@student.42wolfsburg.de>
2022-12-18 14:57:09 +0100

    added ft_strchr.c

--- functions/ft_strchr.c
+++ functions/ft_strchr.c
@@ -6,16 +6,37 @@
 /*   By: gabd-el- <gabd-el-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 12:54:33 by gabd-el-          #+#    #+#             */
-/*   Updated: 2022/12/15 12:54:35 by gabd-el-         ###   ########.fr       */
+/*   Updated: 2022/12/18 14:56:03 by gabd-el-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char *strchr(const char *s, int c)
+char *ft_strchr(const char *s, int c)
 {
-    unsigned char x;
+    char x;
 
-    x = (unsigned char) c;
-    
+    x = (char) c;
+    while (*s != '\0')
+    {
+        if (*s == x)
+        {
+            return ((char *)s);
+        }
+        s++;
+    }
+    return (char *)NULL;
+
 }
+
+/*#include <stdio.h>
+int main() 
+{
+    const char *str = "heea";
+    char  c;
+   
+    c = 'w';
+    printf("Found letter:%c\n", *ft_strchr(str, c));
+    //*ft_strchr(str, c);
+    return (0);
+}*/
