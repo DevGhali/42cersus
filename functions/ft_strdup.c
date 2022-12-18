ebc1bc3e566376173bcea1f6f97e656229f11d4b
gabd-el- <gabd-el-@student.42wolfsburg.de>
2022-12-18 12:32:46 +0000

    added some new functions

--- functions/ft_strdup.c
+++ functions/ft_strdup.c
@@ -1,28 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/17 15:51:37 by gabd-el-          #+#    #+#             */
-/*   Updated: 2022/12/18 12:00:28 by codespace        ###   ########.fr       */
+/*   Created: 2022/12/18 12:02:41 by codespace         #+#    #+#             */
+/*   Updated: 2022/12/18 12:32:26 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
 #include "libft.h"
+#include <unistd.h>
 
-void *ft_calloc(size_t nmemb, size_t size)
+char *strdup(const char *s)
 {
-    void *ptr;
-    
-    if (nmemb == 0 || size == 0)
+    char *dup;
+    size_t s_len;
+    size_t i;
+
+    i = 0;
+    s_len = ft_strlen(s);
+    dup = malloc(sizeof(char) * (s_len + 1));
+    if (dup == NULL)
         return (NULL);
-    ptr = malloc(nmemb * size);
-    if (ptr == NULL)
-        return (ptr);
-    ft_bzero(ptr, size * nmemb);
-    return (ptr);
+    else
+    {
+        while (i < s_len)
+        {
+            dup[i] = s[i];
+            i++;
+        }
+        dup[i] = "\0";
+    }
+    return (dup);
 }
